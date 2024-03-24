42340c00ea07c43dadb157d6dd26ab2bec9e31d7
Skeletus <Samol_88@hotmail.com>
2024-03-23 19:22:50 -0500

    adding admin attribute to user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,11 @@ const userSchema = new mongoose.Schema({
     profilePicture:{
         type: String,
         default: "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png",
-    }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 }, {timestamps: true}
 );
 
