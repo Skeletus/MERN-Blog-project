55047ef3516c41e3aa06e17ea66c7b8ef5012c61
Skeletus <Samol_88@hotmail.com>
2024-03-26 16:59:24 -0500

    added show users in admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import Dashsidebar from '../components/Dashsidebar';
 import Dashprofile from '../components/Dashprofile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -26,5 +27,7 @@ export default function Dashboard() {
     { tab === 'profile' && <Dashprofile />}
     {/* posts... */}
     { tab === 'posts' && <DashPosts />}
+    {/* users */}
+    { tab === 'users' && <DashUsers />}
   </div>
 }
